f28dfed6b80abed908372c0d30fe5057a7c87ce5
SwadhiSankar <swadhis14@gmail.com>
2024-09-03 13:54:09 +0530

    deferencing and mutating(changing original value ) in pointers

--- pointers.go
+++ pointers.go
@@ -9,10 +9,11 @@ func main(){
 	fmt.Println("pointer",*agePointer) //value behind the ptr
 	fmt.Println("Age",age) 
 
-	adultYears := getAdultYears(agePointer) //delete the copy after executing this
-	fmt.Println("Adult years ",adultYears)
+	 getAdultYears(agePointer) //delete the copy after executing this
+	fmt.Println("Adult years ",age) //overrides the value and return as 5 
 }
 
-func getAdultYears(age *int) int { //  copy value 
- return *age - 18
+func getAdultYears(age *int)  { //  copy value 
+//  return *age - 18
+*age = *age-18
 }
